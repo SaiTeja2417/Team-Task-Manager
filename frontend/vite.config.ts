43f4e671ada291ee326b515d0e32e72fbd30509d
@tsai58167 <tsai58167@gmail.com>
2026-05-02 20:03:23 +0530

    fix vite config __dirname issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 
+const __dirname = new URL('.', import.meta.url).pathname
 
 function figmaAssetResolver() {
   return {
@@ -18,17 +19,12 @@ function figmaAssetResolver() {
 export default defineConfig({
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
